3b4535cbf3ac2463247c99a981ab560e0d9487fe
dongjoonseo <ddoonngg2@naver.com>
2023-10-20 20:34:47 +0900

    2023.10.20

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+};
+npm install react-native-screens react-native-safe-area-context
---
+++ screen/DetailScreen.js
@@ -0,0 +1,12 @@
+mport React from 'react';
+import {View, Text} from 'react-native';
+
+function DetailScreen() {
+  return (
+    <View>
+      <Text>Detail</Text>
+    </View>
+  );
+}
+
+export default DetailScreen;
