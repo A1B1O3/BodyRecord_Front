8101edde83edd8f3928e22afc9928ae8ca346f49
dongjoonseo <ddoonngg2@naver.com>
2023-10-28 23:36:19 +0900

    23.10.28

---
+++ src/pages/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Login() {
+    return (
+        <div>
+            <p>
+            로그인 페이지
+            </p>
+        </div>
+    );
+
+}
+
+export default Login;
---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Main() {
+    return (
+        <div>
+            <p>
+            메인 페이지
+            </p>
+        </div>
+    );
+
+}
+
+export default Main;
